eeabf96b411ee0534ca0297e16bff269d7dc29f4
cox <coxleehubilla@gmail.com>
2025-10-06 21:16:22 +0800

    feat(async): transferred to async from functions

--- src/function/asyncCompose.ts
+++
@@ -1,7 +0,0 @@
-export function asyncCompose(...fns: ((...args: any[]) => Promise<any>)[]) {
-  return async (input: any) => {
-    let result = input;
-    for (let i = fns.length - 1; i >= 0; i--) result = await fns[i](result);
-    return result;
-  };
-}
