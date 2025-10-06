ff168f3073358af99f53e9437604aac96c718d29
cox <coxleehubilla@gmail.com>
2025-10-06 14:12:29 +0800

    chore: clean up duplicate functions

--- src/function/timeout.ts
+++
@@ -1,8 +0,0 @@
-export function timeout<T>(promise: Promise<T>, ms: number): Promise<T> {
-  return Promise.race([
-    promise,
-    new Promise<never>((_, reject) =>
-      setTimeout(() => reject(new Error("Timeout exceeded")), ms)
-    ),
-  ]);
-}
--- test/function/timeout.test.ts
+++
@@ -1,8 +0,0 @@
-import { timeout } from "../../src/function/timeout";
-
-describe("timeout", () => {
-  it("should reject promise after timeout", async () => {
-    const slow = () => new Promise((r) => setTimeout(() => r("ok"), 100));
-    await expect(timeout(slow(), 50)).rejects.toThrow();
-  });
-});
